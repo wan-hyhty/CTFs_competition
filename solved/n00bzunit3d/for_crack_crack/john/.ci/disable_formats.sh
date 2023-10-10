2bc74aaafaf81d33231ce4739b26fcad0df5a6b7
wan <111769169+wan-hyhty@users.noreply.github.com>
2023-10-10 09:41:02 +0700

    a

---
+++ solved/n00bzunit3d/for_crack_crack/john/.ci/disable_formats.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -e
+
+# There was a bug in echo -e in Travis
+# TODO: we know these formats must be fixed (or removed)
+echo '[Local:Disabled:Formats]' > ../run/john-local.conf
+echo 'Raw-SHA512-free-opencl = Y' >> ../run/john-local.conf
+echo 'XSHA512-free-opencl = Y' >> ../run/john-local.conf
+echo 'gpg-opencl = Y' >> ../run/john-local.conf
+echo 'KeePass-opencl = Y' >> ../run/john-local.conf
+
+# These formats fail OpenCL CPU runtime
+echo 'RAR-opencl = Y' >> ../run/john-local.conf
+
+# These formats run very slowly inside CI
+# Time measures are from Intel CPU driver running inside Docker
+echo 'ansible-opencl = Y' >> ../run/john-local.conf    # (282.202952 secs) PASS
+echo 'bitlocker-opencl = Y' >> ../run/john-local.conf  # (146.356993 secs) PASS
+echo 'bitwarden-opencl = Y' >> ../run/john-local.conf  # (275.649462 secs) PASS
+echo 'ethereum-opencl = Y' >> ../run/john-local.conf   # (468.007940 secs) PASS
+echo 'FVDE-opencl = Y' >> ../run/john-local.conf       # (184.908568 secs) PASS
+echo 'lp-opencl = Y' >> ../run/john-local.conf         # (177.392942 secs) PASS
+echo 'notes-opencl = Y' >> ../run/john-local.conf      # (179.212104 secs) PASS
+echo 'PBKDF2-HMAC-SHA256-opencl = Y' >> ../run/john-local.conf  # (189.267312 secs) PASS
+
+# Other interesting formats
+# Testing: RAR5-opencl [PBKDF2-SHA256 OpenCL] (70.553782 secs) PASS
+# Testing: ODF-opencl, OpenDocument Star/Libre/OpenOffice [PBKDF2-SHA1 BF/AES OpenCL] (97.022575 secs) PASS
+# Testing: diskcryptor-opencl, DiskCryptor [PBKDF2-SHA512 OpenCL] (71.142642 secs) PASS
+# Testing: diskcryptor-aes-opencl, DiskCryptor AES XTS (only) [PBKDF2-SHA512 AES OpenCL] (75.497302 secs) PASS
+# Testing: EncFS-opencl [PBKDF2-SHA1 AES OpenCL] (82.412256 secs) PASS
+# Testing: ethereum-presale-opencl, Ethereum Presale Wallet [PBKDF2-SHA256 AES Keccak OpenCL] (125.017417 secs) PASS
---
+++ solved/n00bzunit3d/for_crack_crack/john/.travis/travis-ci.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+# if requested, run all tests
+if [[ "$TRAVIS_PULL_REQUEST_BRANCH" != "force-travis" ]]; then
+
+    # on the main branch, run all tests
+    if [[ "$TRAVIS_BRANCH" != "bleeding-jumbo" || "$TRAVIS_PULL_REQUEST" =~ ^[0-9]+$ ]]; then
+
+        # otherwise, run only Linux jobs for now
+        if [[ "$TRAVIS_OS_NAME" != "linux" ]]; then
+            echo '---------------------------------- Skipping CI ---------------------------------'
+            echo 'In order to spare resources, skip Travis CI build.'
+            echo '--------------------------------------------------------------------------------'
+
+            # Nothing to do.
+            exit 0
+        fi
+        echo 'Proceeding with build'
+    fi
+fi
+
+# Need a docker image to run the tests
+if [[ "$DOCKER" == "yes" ]]; then
+    docker run --cap-add SYS_PTRACE -v "$(pwd)":/cwd claudioandre/john:opencl sh -c \
+      "
+        cd /cwd; \
+        export OPENCL=$OPENCL; \
+        export CC=$TRAVIS_COMPILER; \
+        export BUILD_OPTS='$BUILD_OPTS'; \
+        echo; \
+        $0;
+      "
+    exit $?
+fi
+
+# ---- Build and test JtR ----
+cd src
+
+# The testing binary
+JTR=../run/john
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    ./configure --enable-werror CPPFLAGS="-I/usr/local/opt/openssl/include" LDFLAGS="-L/usr/local/opt/openssl/lib"
+    make -sj4
+else
+    # Address sanitizer instrumented code
+    export ASAN_OPTIONS=symbolize=1
+    export ASAN_SYMBOLIZER_PATH=$(which llvm-symbolizer)
+
+    if [[ "$OPENCL" == "yes" && "$TRAVIS_DIST" == "trusty" ]]; then
+        # Fix the OpenCL stuff
+        mkdir -p /etc/OpenCL/vendors
+        sudo ln -sf /usr/lib/fglrx/etc/OpenCL/vendors/amdocl64.icd /etc/OpenCL/vendors/amd.icd
+    fi
+
+    # Configure and build
+    ./configure --enable-werror $BUILD_OPTS
+    make -sj4
+fi
+
+# Disable problematic formats before testing
+source ../.ci/disable_formats.sh
+
+echo '---------------------------------- Build Info ----------------------------------'
+$JTR --list=build-info
+echo '--------------------------------------------------------------------------------'
+
+if [[ "$OPENCL" == "yes" ]]; then
+    echo '---------------------------------- OpenCL Info ----------------------------------'
+    $JTR --list=opencl-devices
+    echo '---------------------------------------------------------------------------------'
+fi
+
+# Except for MacOS, split tests
+if [[ "$OPENCL" == "yes" && "$TRAVIS_OS_NAME" != "osx" ]]; then
+    echo '== Running $JTR -test=0 --format=opencl =='
+    $JTR -test=0 --format=opencl
+else
+    echo '== Running $JTR -test=0 =='
+    $JTR -test=0
+fi
+
