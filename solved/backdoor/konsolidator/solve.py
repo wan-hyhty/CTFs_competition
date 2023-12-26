81bca521e9c6b98e38ab7b6f849a049549fec6e3
wan <111769169+wan-hyhty@users.noreply.github.com>
2023-12-27 02:45:57 +0700

    a

---
+++ solved/backdoor/konsolidator/solve.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8aa477b49a72076cc671963abb9f1983e65ca7d96b77c84879b9791c7b21c6a8
+size 1715
---
+++ solved/backdoor/master_formatter_v2/solve.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:efa5be59b61c966d894da459255226a6ea3695f86a06d248c3c920022227cf4c
+size 443
